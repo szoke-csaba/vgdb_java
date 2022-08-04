cf982571c4dd07830f1153aae2eebfd74d8a1ef3
Csaba Szőke <49896750+szoke-csaba@users.noreply.github.com>
2022-08-04 20:35:10 +0200

    Created project.

---
+++ src/main/java/io/github/szokecsaba/vgdb/VgdbApplication.java
@@ -0,0 +1,13 @@
+package io.github.szokecsaba.vgdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VgdbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/szokecsaba/vgdb/VgdbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.szokecsaba.vgdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
